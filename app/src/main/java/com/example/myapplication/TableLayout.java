fd8e59c45fa809310a87baa58f5a41e0a0c3408f
REZU86 <yassir.mahboub.my@gmail.com>
2022-11-24 19:31:52 +0100

    actividad radiocheck

--- app/src/main/java/com/example/myapplication/TableLayout.java
+++ app/src/main/java/com/example/myapplication/TableLayout.java
@@ -19,7 +19,7 @@ public class TableLayout extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TableLayout.this,  CualquierLayout.class);
+                Intent intent = new Intent(TableLayout.this,  ButtonsExtended.class);
                 startActivity(intent);
             }
         });
